ba8fa1ab06e38663f929fd54db2f52765448b213
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-13 16:41:49 +0530

    Removed All Imports

--- src/main/java/com/interview/brushups/kafka/ProducerClient.java
+++ src/main/java/com/interview/brushups/kafka/ProducerClient.java
@@ -1,6 +1,10 @@
 package com.interview.brushups.kafka;
 
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;

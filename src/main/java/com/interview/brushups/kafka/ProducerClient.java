97cf349429d446e2fb17d7d2bf43ce328b36ef92
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-13 16:36:30 +0530

    Updated Producer Kafka Code

--- src/main/java/com/interview/brushups/kafka/ProducerClient.java
+++ src/main/java/com/interview/brushups/kafka/ProducerClient.java
@@ -1,8 +1,6 @@
 package com.interview.brushups.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -33,7 +31,7 @@ public class ProducerClient {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // Create a Kafka Producer, put in the configuration properties.
-        KafkaProducer < String, String > producer = new KafkaProducer < String, String > (props);
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 
         //Assign topicName to string variable
         String topicName = "hello-topic";
@@ -45,8 +43,25 @@ public class ProducerClient {
         int count = 0;
         for (int i = 1; i <= number; i++) {
             count++;
-            ProducerRecord < String, String > record = new ProducerRecord(topicName, Integer.toString(count), "Hello " + count);
-            producer.send(record);
+
+            //Create a new Producer Record | (topicName, Value) OR (topicName, Key, Value)
+            ProducerRecord<String, String> record;
+            record = new ProducerRecord(topicName, Integer.toString(count), "Hello " + count);
+
+            // Handle sent records
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e != null) {
+                        System.out.println("Record Sent Failed");
+                    } else {
+                        System.out.println("Topic :" + recordMetadata.topic());
+                        System.out.println("Partition :" + recordMetadata.partition());
+                        System.out.println("Offset :" + recordMetadata.offset());
+                        System.out.println("TimeStamp :" + recordMetadata.timestamp());
+                    }
+                }
+            });
         }
         producer.close();
     }

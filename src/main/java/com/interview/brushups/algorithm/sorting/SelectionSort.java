7834ed5e28338440ea16ae7cdb90da563083aaeb
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-24 17:19:31 +0530

    Added comments to Selection Sort

--- src/main/java/com/interview/brushups/algorithm/sorting/SelectionSort.java
+++ src/main/java/com/interview/brushups/algorithm/sorting/SelectionSort.java
@@ -9,22 +9,32 @@ public class SelectionSort {
         int[] array = {34, 36, 2, 9, 47, 7, 1};
         selectionSort(array);
 
-        for(int number : array){
+        for (int number : array) {
             System.out.println(number);
         }
     }
 
     public static void selectionSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
+
+            //store the minimum value as that index
             int min = array[i];
+
+            //also store the index is being worked
             int minId = i;
-            for (int j = i+1; j <= array.length; j++) {
+
+            //run another loop and lookup an element skipping the current element
+            for (int j = i + 1; j < array.length; j++) {
+
+                // if the next element and forward element is shorter than the current minimum
+                // capture the index of it, and store the index
                 if (array[j] < min) {
                     min = array[j];
                     minId = j;
                 }
             }
-            // swapping
+
+            // now we have the selected the shortest element, exchange it with the current index
             int temp = array[i];
             array[i] = min;
             array[minId] = temp;

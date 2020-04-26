6f0d7674afb5456ebd82919ca9cc6bca1f8f30e6
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-26 13:40:10 +0530

    Added usage readResolve() method

--- src/main/java/com/interview/brushups/marker/serializable/Car.java
+++ src/main/java/com/interview/brushups/marker/serializable/Car.java
@@ -9,7 +9,6 @@ import java.io.Serializable;
  * 3. Static data members and transient data members are not saved via Serialization process.
  * So, if you don’t want to save value of a non-static data member then make it transient.
  * 4. Constructor of object is never called when an object is de-serialized.
- *
  */
 public class Car implements Serializable {
 
@@ -38,4 +37,11 @@ public class Car implements Serializable {
                 ", password='" + password + '\'' +
                 '}';
     }
+
+    /*
+     * While we deserialize, readResolve() method will be used to deserialize the object.
+     * private Object readResolve()  {
+     *      return new Car(23,"Ferrai", "f3r@rI'");
+     *  }
+     */
 }

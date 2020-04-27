3385430abf6cda4fe8576cb3a6f4f4ddf338afa0
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-27 15:30:26 +0530

    Corrected Formatting

--- src/main/java/com/interview/brushups/askedprograms/FindSecondLargest.java
+++ src/main/java/com/interview/brushups/askedprograms/FindSecondLargest.java
@@ -7,6 +7,7 @@ public class FindSecondLargest {
 
     public static void main(String[] args) {
         int array[] = {1, 9, 5, 11, 34, 38};
+
         int result = secondLargest(array);
         if (result == Integer.MIN_VALUE) {
             System.out.println("No Second Largest Element Found");
@@ -19,35 +20,33 @@ public class FindSecondLargest {
         System.out.println("Second Largest Value is : " + result);
     }
 
-    /* Function to print the second largest
-    elements */
+    /**
+     * Method to print the second largest elements
+     */
     public static int secondLargest(int array[]) {
         int first = Integer.MIN_VALUE;
         int second = Integer.MIN_VALUE;
 
-        int array_size = array.length;
+        int n = array.length;
 
-        /**
-         * There should be at-least two element in the array
-         */
-        if (array_size < 2) {
+        // There should be at-least two element in the array
+        if (n < 2) {
             return -1;
         }
 
-        for (int i = 0; i < array_size; i++) {
-			/* If current element is greater than, first then update both first and second
-			 */
+        for (int i = 0; i < n; i++) {
+
+            // If current element is greater than, first then update both first and second
             if (array[i] > first) {
                 // store the previous max value to second variable
                 second = first;
                 first = array[i];
             }
 
-            /* If arr[i] is greater than second, and not equals to first, update second */
+            // If arr[i] is greater than second, and not equals to first, update second */
             else if (array[i] > second && array[i] != first)
                 second = array[i];
         }
         return second;
     }
 }
-

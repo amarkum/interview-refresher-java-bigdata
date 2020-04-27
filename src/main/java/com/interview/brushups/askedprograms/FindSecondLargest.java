136fe61736b0879d3ebc9ff29bbc01a5a58439db
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-27 15:24:32 +0530

    Added Program to Find Second Largest in the array

---
+++ src/main/java/com/interview/brushups/askedprograms/FindSecondLargest.java
@@ -0,0 +1,53 @@
+package com.interview.brushups.askedprograms;
+
+/**
+ * Program to Find Second Largest in an Array - Accolite
+ */
+public class FindSecondLargest {
+
+    public static void main(String[] args) {
+        int array[] = {1, 9, 5, 11, 34, 38};
+        int result = secondLargest(array);
+        if (result == Integer.MIN_VALUE) {
+            System.out.println("No Second Largest Element Found");
+            return;
+        }
+        if (result == -1) {
+            System.out.println("Array Should have at-least two element");
+            return;
+        }
+        System.out.println("Second Largest Value is : " + result);
+    }
+
+    /* Function to print the second largest
+    elements */
+    public static int secondLargest(int array[]) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        int array_size = array.length;
+
+        /**
+         * There should be at-least two element in the array
+         */
+        if (array_size < 2) {
+            return -1;
+        }
+
+        for (int i = 0; i < array_size; i++) {
+			/* If current element is greater than, first then update both first and second
+			 */
+            if (array[i] > first) {
+                // store the previous max value to second variable
+                second = first;
+                first = array[i];
+            }
+
+            /* If arr[i] is greater than second, and not equals to first, update second */
+            else if (array[i] > second && array[i] != first)
+                second = array[i];
+        }
+        return second;
+    }
+}
+

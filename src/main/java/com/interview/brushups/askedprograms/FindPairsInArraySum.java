37a7ede36e6dcc2839404bc72e61bdd0d0a0e5cf
Amar Kumar <amar.mailsbox@gmail.com>
2020-04-30 01:00:19 +0530

    Finding Pair for Given Sum

--- src/main/java/com/interview/brushups/askedprograms/FindPairsInArraySum.java
+++ src/main/java/com/interview/brushups/askedprograms/FindPairsInArraySum.java
@@ -4,23 +4,32 @@ import java.util.HashSet;
 
 /**
  * Find Pairs in an array, whose sum is X
+ * Complexity - O(n), since we traverse the array only once
  */
 public class FindPairsInArraySum {
 
     static void findPairs(int arr[], int sum) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < arr.length; ++i) {
-            int temp = sum - arr[i];
+        HashSet<Integer> set = new HashSet();
 
-            if (set.contains(temp)) {
-                System.out.println(" [" + arr[i] + ", " + temp + "]");
+        for(int i=0; i < arr.length; i++){
+
+            // calculate the required number which will form the sum in set
+            int required = sum - arr[i];
+
+            // if the required number is present in set, current & required number will be a pair
+            if(set.contains(required)){
+                System.out.println("["+required+", "+arr[i]+"]");
             }
-            set.add(arr[i]);
+
+            // if required number is not present, add the current number to set.
+            else {
+                set.add(arr[i]);
+            }
         }
     }
 
     public static void main(String[] args) {
-        int A[] = {1, 4, 45, 6, 10, 8, 8, 12};
+        int A[] = {1, 4, 45, 6, 10, 8, 8, 7, 12};
         int n = 16;
         findPairs(A, n);
     }
